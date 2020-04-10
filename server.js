ac4106b66eba14ce77048bd6a26d845046ce08cb
kimeunyoung01 <rola70@nate.com>
2020-04-10 12:51:37 +0900

    node oracle insert

--- server.js
+++ server.js
@@ -5,7 +5,48 @@ app.use(express.static("public"));
 app.use(express.bodyParser());
 app.use(app.router);
 
-//부서등록 기능을 추가해 봅니다.
+app.get("/insertDept", function(req,res){
+  var dno = req.param("dno");
+  var dname = req.param("dname");
+  var dloc = req.param("dloc");
+
+  var sql = "insert into dept values("+dno+", '"+dname+"', '"+dloc+"')";
+  const oracledb = require('oracledb');
+  oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+  const mypw = "madang"  // set mypw to the hr schema password
+  async function run() {
+    let connection;
+    try {
+      connection = await oracledb.getConnection(  {
+        user          : "c##madang",
+        password      : mypw,
+        connectString : "localhost:1521/XE"
+      });
+
+      console.log(sql);
+      const result = await connection.execute(sql);
+      connection.commit();
+    //  console.log(result);
+      console.log(result.rowsAffected);
+      res.send("1");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  }
+
+  run();
+
+});
+
+//부서수정, 삭제기능을 추가 해 봅니다.
 app.get("/listDept", function(req,res){
   const oracledb = require('oracledb');
   oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
@@ -20,7 +61,7 @@ app.get("/listDept", function(req,res){
       });
 
       const result = await connection.execute(
-        `select * from dept`
+        `select * from dept order by dno`
       );
     //  console.log(result);
       console.log(result.rows);
